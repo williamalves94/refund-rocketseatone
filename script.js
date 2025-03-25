a9311353886cac5ea93652e35bb2f42028aac15e
William Alves <williamcostacardoso94@gmail.com>
2025-03-25 16:51:00 -0300

    feat: validando input com regex

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const amount = document.getElementById("amount");
 
-// oninput dispara sempre que há uma alteração no valor (em tempo real).
+// oninput dispara sempre que há uma alteração no valor (em tempo real)
 amount.oninput = () => {
   // O replace() no JavaScript é usado para substituir partes de uma string por outra. Ele pode funcionar com uma string ou uma expressão regular.
   // fazendo com que o input só aceite numeros e nao letras com regex
