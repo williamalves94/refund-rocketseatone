15e822128b40da41ff45102edb6353ff49a05820
William Alves <williamcostacardoso94@gmail.com>
2025-03-26 17:26:36 -0300

    feat: adicionando items a lista

--- script.js
+++ script.js
@@ -61,10 +61,25 @@ function expenseAdd(newExpense) {
     expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
     expenseIcon.setAttribute("alt", newExpense.category_name);
 
-    //adiciona as informações no item
-    expenseItem.append(expenseIcon);
+    // cria a info da despesa
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
 
-    //adiciona o item na lista
+    // cria o nome da despesa
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    // cria a categoria da despesa
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    // adiciona name e category na div das info das dispesa
+    expenseInfo.append(expenseName, expenseCategory);
+
+    // adiciona as informações no item
+    expenseItem.append(expenseIcon, expenseInfo);
+
+    // adiciona o item na lista
     expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
